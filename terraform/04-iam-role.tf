b1448278c75f364ef5b511846c010bffcc7d5cb7
spartzames <spartzames@gmail.com>
2023-04-20 21:14:47 +0900

    add cross account

--- terraform/04-iam-role.tf
+++ terraform/04-iam-role.tf
@@ -18,3 +18,29 @@ resource "aws_iam_role_policy_attachment" "EC2S3ReadOnly_kthong-attach" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
   role       = aws_iam_role.EC2S3ReadOnly_kthong.name
 }
+
+data "aws_iam_policy_document" "EC2PowerUser_policy_kthong" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::021544540902:role/CCOE"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "EC2PowerUser_kthong" {
+  name = "EC2PowerUser_kthong"
+  assume_role_policy = data.aws_iam_policy_document.EC2PowerUser_policy_kthong.json
+  tags = {
+    "contractors-assume-role" : true
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2FullAccess_kthong-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  role       = aws_iam_role.EC2PowerUser_kthong.name
+}
