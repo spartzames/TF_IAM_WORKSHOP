359da2b3e5bed200662c9ca04d2763e4fa938771
spartzames <spartzames@gmail.com>
2023-04-20 20:57:05 +0900

    fix

--- terraform/04-iam-role.tf
+++ terraform/04-iam-role.tf
@@ -1,6 +1,6 @@
 data "aws_iam_policy_document" "ec2-s3-read-only-policy-kthong" {
   statement {
-    actions = ["AmazonS3ReadOnlyAccess"]
+    actions = ["sts:AssumeRole"]
 
     principals {
       type        = "Service"
@@ -12,4 +12,9 @@ data "aws_iam_policy_document" "ec2-s3-read-only-policy-kthong" {
 resource "aws_iam_role" "EC2S3ReadOnly_kthong" {
   name               = "EC2S3ReadOnly_kthong"
   assume_role_policy = data.aws_iam_policy_document.ec2-s3-read-only-policy-kthong.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks-fargate-profile" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  role       = aws_iam_role.EC2S3ReadOnly_kthong.name
 }
