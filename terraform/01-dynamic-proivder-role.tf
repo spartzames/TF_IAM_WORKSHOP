0410663965ce4c282ca7ac274ce51657730cae58
spartzames <spartzames@gmail.com>
2023-04-20 13:24:41 +0900

    fix

--- terraform/01-dynamic-proivder-role.tf
+++ terraform/01-dynamic-proivder-role.tf
@@ -1,9 +1,9 @@
 resource "aws_iam_policy" "DepartmentalEC2AccessPolicy" {
-    policy = file("./../env/DepartmentalEC2AccessPolicy.json")
+    policy = file("./DepartmentalEC2AccessPolicy.json")
     name = "DepartmentalEC2AccessPolicy"
 }
 
 resource "aws_iam_policy" "ContractorRoleAssumptionPolicy" {
-    policy = file("./../env/ContractorRoleAssumptionPolicy.json")
+    policy = file("./ContractorRoleAssumptionPolicy.json")
     name = "ContractorRoleAssumptionPolicy"
 }
