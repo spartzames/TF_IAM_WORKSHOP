98cbaf6aeb7e6c4be9a2b99ffb5f4f677f8b86ca
spartzames <spartzames@gmail.com>
2023-04-20 13:22:52 +0900

    add ex1

--- terraform/01-dynamic-proivder-role.tf
+++ terraform/01-dynamic-proivder-role.tf
@@ -1,29 +1,9 @@
-resource "aws_iam_role" "tfc-kthong-role" {
-  name = "tfc-kthong-role"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Effect = "Allow"
-      Principal = {
-        Federated = "arn:aws:iam::789535401130:oidc-provider/app.terraform.io"
-      }
-      Condition = {
-        StringEquals = {
-            "app.terraform.io:aud" = "aws.workload.identity"
-        }
-        StringLike = {
-            "app.terraform.io:sub" = [
-                "organization:KAL_DevOps_PoC:project:kthong_prj:workspace:TF_IAM_WORKSHOP:run_phase:*"
-            ]
-        }
-      }
-    }]
-    Version = "2012-10-17"
-  })
+resource "aws_iam_policy" "DepartmentalEC2AccessPolicy" {
+    policy = file("./../env/DepartmentalEC2AccessPolicy.json")
+    name = "DepartmentalEC2AccessPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "tfc-kthong-role" {
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-  role       = aws_iam_role.tfc-kthong-role.name
+resource "aws_iam_policy" "ContractorRoleAssumptionPolicy" {
+    policy = file("./../env/ContractorRoleAssumptionPolicy.json")
+    name = "ContractorRoleAssumptionPolicy"
 }
