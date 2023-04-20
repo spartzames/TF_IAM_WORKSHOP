19c9459ba9ecec8d9715cd1b79f651a03413ef42
spartzames <spartzames@gmail.com>
2023-04-20 14:08:36 +0900

    fix

--- terraform/02-users.tf
+++ terraform/02-users.tf
@@ -2,15 +2,15 @@ resource "aws_iam_user" "kthong-iam-workshop-01" {
   name = "kthong-iam-workshop-01"  
   path = "/"
   force_destroy = true
+  tags = {
+    "department" : "HR"
+  }
 }
 
 resource "aws_iam_user_login_profile" "kthong-iam-workshop-01-profile" {
   user    = aws_iam_user.kthong-iam-workshop-01.name
   pgp_key = var.pgp_key
   password_reset_required = true
-  tags = {
-    "department" : "HR"
-  }
 }
 
 resource "aws_iam_user_policy_attachment" "kthong-iam-workshop-01-policy-attach" {
@@ -19,22 +19,22 @@ resource "aws_iam_user_policy_attachment" "kthong-iam-workshop-01-policy-attach"
 }
 
 output "kthong-iam-workshop-01-password" {
-  value = aws_iam_user_login_profile.kthong-iam-workshop-01-profile.encrypted_password
+  value = aws_iam_user_login_profile.kthong-iam-workshop-01-profile.kthong-iam-workshop-01-profile-encrypted_password
 }
 
 resource "aws_iam_user" "kthong-iam-workshop-02" {
   name = "kthong-iam-workshop-02"  
   path = "/"
   force_destroy = true
+  tags = {
+    "department" : "Finance"
+  }
 }
 
 resource "aws_iam_user_login_profile" "kthong-iam-workshop-02-profile" {
   user    = aws_iam_user.kthong-iam-workshop-02.name
   pgp_key = var.pgp_key
   password_reset_required = true
-  tags = {
-    "department" : "Finance"
-  }
 }
 
 resource "aws_iam_user_policy_attachment" "kthong-iam-workshop-02-policy-attach" {
@@ -43,7 +43,7 @@ resource "aws_iam_user_policy_attachment" "kthong-iam-workshop-02-policy-attach"
 }
 
 output "kthong-iam-workshop-02-password" {
-  value = aws_iam_user_login_profile.kthong-iam-workshop-02-profile.encrypted_password
+  value = aws_iam_user_login_profile.kthong-iam-workshop-02-profile.kthong-iam-workshop-02-profile-encrypted_password
 }
 
 output "keybase_password_decrypt_command" {
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,6 @@
 locals {
-  encrypted_password               = join("", aws_iam_user_login_profile.example.*.encrypted_password)
+  kthong-iam-workshop-01-profile-encrypted_password               = join("", aws_iam_user_login_profile.kthong-iam-workshop-01-profile.*.encrypted_password)
+  kthong-iam-workshop-02-profile-encrypted_password               = join("", aws_iam_user_login_profile.kthong-iam-workshop-02-profile.*.encrypted_password)
   pgp_key_is_keybase               = length(regexall("keybase:", var.pgp_key)) > 0 ? true : false
   keybase_password_decrypt_command = local.pgp_key_is_keybase ? templatefile("templates/keybase_password_decrypt_command.sh", { encrypted_password = local.encrypted_password }) : ""
 }
